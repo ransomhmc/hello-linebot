0ce71052829313b8fbe72b1a45d8285e0d6e9aaf
Raymond <ransomhmc@gmail.com>
2019-01-31 00:26:17 +0800

    aaaaa

---
+++ config.js
@@ -0,0 +1,68 @@
+const nconf = module.exports = require('nconf');
+const path = require('path');
+
+nconf
+  // 1. Command-line arguments
+  .argv()
+  // 2. Environment variables
+  .env([
+    'CLOUD_BUCKET',
+    'DATA_BACKEND',
+    'GCLOUD_PROJECT',
+    'INSTANCE_CONNECTION_NAME',
+    'MONGO_URL',
+    'MONGO_COLLECTION',
+    'MONGO_DB_NAME',
+    'MYSQL_USER',
+    'MYSQL_PASSWORD',
+    'NODE_ENV',
+    'PORT'
+  ])
+  // 3. Config file
+  .file({ file: path.join(__dirname, 'config.json') })
+  // 4. Defaults
+  .defaults({
+    // Typically you will create a bucket with the same name as your project ID.
+    CLOUD_BUCKET: 'gs://linebot-37er/',
+
+    // dataBackend can be 'datastore', 'cloudsql', or 'mongodb'. Be sure to
+    // configure the appropriate settings for each storage engine below.
+    // If you are unsure, use datastore as it requires no additional
+    // configuration.
+    DATA_BACKEND: 'datastore',
+
+    // This is the id of your project in the Google Cloud Developers Console.
+    GCLOUD_PROJECT: 'linebot-37er',
+
+    // MongoDB connection string
+    // https://docs.mongodb.org/manual/reference/connection-string/
+    MONGO_URL: 'mongodb://localhost:27017',
+    MONGO_COLLECTION: 'books',
+
+    MYSQL_USER: '',
+    MYSQL_PASSWORD: '',
+
+    PORT: 8080
+  });
+
+// Check for required settings
+checkConfig('GCLOUD_PROJECT');
+checkConfig('CLOUD_BUCKET');
+
+if (nconf.get('DATA_BACKEND') === 'cloudsql') {
+  checkConfig('MYSQL_USER');
+  checkConfig('MYSQL_PASSWORD');
+  if (nconf.get('NODE_ENV') === 'production') {
+    checkConfig('INSTANCE_CONNECTION_NAME');
+  }
+} else if (nconf.get('DATA_BACKEND') === 'mongodb') {
+  checkConfig('MONGO_URL');
+  checkConfig('MONGO_COLLECTION');
+  checkConfig('MONGO_DB_NAME');
+}
+
+function checkConfig (setting) {
+  if (!nconf.get(setting)) {
+    throw new Error(`You must set ${setting} as an environment variable or in config.json!`);
+  }
+}
